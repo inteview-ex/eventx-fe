6b7fe580c18a11b9056762132019d43b29667f15
jackykwandesign <jackykwandesign@gmail.com>
2022-09-15 03:36:59 +0000

    remove logic from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { CryptoInfo } from "./assets/types/backend.type";
 import CryptoTicker from "./components/cryptoTicker";
-import { Manager } from "socket.io-client";
+import { useCryptoTickerContext, WithCryptoTickerContext } from "./contexts/cryptoTickerContext";
+
 function App() {
-	const axiosInstance = axios.create({
-		baseURL: process.env.REACT_APP_BACKEND_URL,
-		timeout: 3000,
-	});
-	async function handleGetTickers() {
-		try {
-			const result = await axiosInstance.get<CryptoInfo[]>(
-				`/crypto-ticker?tokens=BTC,ETH,LTC,XMR,XRP,DOGE,DASH,MAID,LSK,SJCX`
-			);
-			console.log("result", result.data);
-			setCryptoInfos(result.data ?? []);
-		} catch (error) {
-			console.log("error", error);
-		}
-	}
-	const socketManager = new Manager(process.env.REACT_APP_BACKEND_URL);
-	socketManager.on("open", () => {
-		console.log("Manager connect");
-	});
-	socketManager.on("close", (reason) => {
-		console.log("Manager close", reason);
-	});
-	socketManager.on("packet", (packet) => {
-		console.group("Manager packet");
-		console.dir(packet, { depth: null });
-		console.groupEnd();
-	});
-	socketManager.on("ping", () => {
-		console.log("Manager ping");
-	});
-	socketManager.on("reconnect", (attempt) => {
-		console.log("Manager Reconnect success: ", attempt);
-	});
-	socketManager.on("reconnect_attempt", (attempt) => {
-		console.log("Manager Reconnect attempt: ", attempt);
-	});
-	socketManager.on("reconnect_error", (err) => {
-		console.error("Manager Reconnect err: ", err);
-	});
-	socketManager.on("reconnect_failed", () => {
-		console.error("Manager Reconnect failed");
-	});
-	const socket = socketManager.socket("/cryptoTicker");
-	socket.on("connect", () => {
-		console.log(`Socket connected`);
-	});
-
-	socket.on("disconnect", (reason) => {
-		console.log(`Socket disconnect due to ${reason}`);
-	});
-
-	socket.on("connect_error", (error) => {
-		console.error("Socket Connect err: ", error);
-	});
-
-	socket.onAny((event, data) => {
-		socketEventHandler(event, data);
-	});
-
-	const socketEventHandler = (event: string, data: any) => {
-		switch (event) {
-			default:
-				console.group("Drop Socket Event");
-				console.log("event", event);
-				console.log("data", data);
-				console.groupEnd();
-		}
-	};
-
-	const [cryptoInfos, setCryptoInfos] = useState<CryptoInfo[]>([]);
-
-	// on page init
+	const {cryptoInfos, sync, isSyncing} = useCryptoTickerContext()
 	useEffect(() => {
-		handleGetTickers();
-	}, []);
-
+		sync()
+	}, [])
+	if(isSyncing){
+		return <div>Loading...</div>
+	}
 	return (
 		<div className="page_container">
 			<div className="ticker_wrapper">
@@ -97,4 +27,4 @@ function App() {
 	);
 }
 
-export default App;
+export default WithCryptoTickerContext(App);
